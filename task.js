63da7c3fd94b7c1108aa1d83b5136ce0af820818
Benjamin "Ewosoft" Efenudu <84343391+benjaminefenudu@users.noreply.github.com>
2021-06-09 13:07:44 +0100

    Added method 2 to task 4

--- task.js
+++ task.js
@@ -26,6 +26,7 @@ alert(withdraw(10000000,500000));
 
 
 // Task 4-----------------
+// Method 1
 numberList = []; sum = 0;
 for (let i=0; i < 20; i++) {
     numberList[i] = (i+1)
@@ -33,4 +34,13 @@ for (let i=0; i < 20; i++) {
 }
 alert(numberList)
 alert(sum)
+
+// Method 2
+// numberList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// sum = 0
+// for (let i=0; i<numberList.length; i++) {
+//    sum = sum + numberList[i]
+// }
+// alert(sum);
+
 
